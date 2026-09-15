3c3d2f7a430c79ba5ae5f03099fb4eeffb7acfcc
Calstar9000 <Calstar9000@gmail.com>
2026-09-15 07:17:24 -0600

    Fix how i did ControlScheme.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ControlScheme.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ControlScheme.java
@@ -4,10 +4,10 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import java.util.function.Supplier;
 
 public class ControlScheme {
-    public static Supplier<Float> DRIVE_STRAFE;
-    public static Supplier<Float> DRIVE_FB;
-    public static Supplier<Float> DRIVE_ROTATE;
-    public static Supplier<Boolean> DRIVE_SLOW_MODE;
+    public Supplier<Float> DRIVE_STRAFE;
+    public Supplier<Float> DRIVE_FB;
+    public Supplier<Float> DRIVE_ROTATE;
+    public Supplier<Boolean> DRIVE_SLOW_MODE;
 
     public void initDriver(Gamepad gamepad1) {
         DRIVE_STRAFE = () -> gamepad1.left_stick_x;
