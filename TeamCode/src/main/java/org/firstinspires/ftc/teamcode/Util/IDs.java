72c215a7dddfd076641677944d36574118d19a50
sylas <sylas.h.jones@gmail.com>
2026-09-18 17:23:01 -0600

    Moving intake to different branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
@@ -8,8 +8,6 @@ public class IDs {
 
     public static final String CONTROL_HUB_IMU = "IMU";
 
-    public static final String INTAKE1_MOTOR_ID= "0";
-    public static final String INTAKE2_MOTOR_ID= "0";
 
     public static final String CONTROL_HUB = "Control hub";
 
