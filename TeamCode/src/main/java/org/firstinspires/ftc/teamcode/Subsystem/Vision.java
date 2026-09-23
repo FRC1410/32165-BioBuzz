dfe44cc1489ffaef65e38d39c244dd0476fb0a75
Calstar9000 <Calstar9000@gmail.com>
2026-09-23 16:45:08 -0600

    Add Vision.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Vision.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Subsystem;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+
+public class Vision {
+    private double tagToCenter = 14.267;
+    private double centerToPivot = 0;
+    private VisionPortal vision_portal;
+
+
+
+    public Vision() {
+
+    }
+
+    private double findDistX
+        (
+            double f,
+            double g,
+            double a,
+            double c
+        )
+    {
+        return 0;
+    }
+}
