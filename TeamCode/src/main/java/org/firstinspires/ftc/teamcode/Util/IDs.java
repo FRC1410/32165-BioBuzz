e82246a70a33693b4b66a5e52855ad12cb0856d0
Calstar9000 <Calstar9000@gmail.com>
2026-09-23 16:47:09 -0600

    Fix a similar issue where subclass was pushed to main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
@@ -11,8 +11,6 @@ public class IDs {
     public static final String INTAKE1_MOTOR_ID= "0";
     public static final String INTAKE2_MOTOR_ID= "0";
 
-    public static final String STORAGE_MOTOR_ID= "0"
-
     public static final String CONTROL_HUB = "Control hub";
 
 }
