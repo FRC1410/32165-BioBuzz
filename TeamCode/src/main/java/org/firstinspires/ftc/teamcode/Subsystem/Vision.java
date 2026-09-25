de9868587a8eb7d74181310f0a47abf7f7a954c7
Calstar9000 <Calstar9000@gmail.com>
2026-09-25 16:42:48 -0600

    Added Vision find distance function, needs pitches, distance from tag, yaw, and which side you are on

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Vision.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Subsystem;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 public class Vision {
-    private double tagToCenter = 14.267;
+    private double tagToCenter = 14.267; // In inches
     private double centerToPivot = 0;
     private VisionPortal vision_portal;
 
@@ -13,14 +13,34 @@ public class Vision {
 
     }
 
-    private double findDistX
+    private double[] findDistX
         (
-            double f,
-            double g,
-            double a,
-            double c
+            double aRobot, // Pitch up from robot
+            double aTag, // Pitch from tag
+            double dist, // Dist cam to tag
+            double yaw, // Yaw
+            boolean sideTop
         )
     {
-        return 0;
+        double a = aRobot + aTag - 90; // This is the angle of the triangle opposite to ours top angle
+        double tagHeight = Math.sin(aRobot) * dist;
+        double robotToCentre =
+                ((tagToCenter * Math.sin(a) * tagHeight) / (Math.sin(a) * tagHeight))
+                        + Math.cos(aRobot) * dist;
+        double x = 84;
+        double y = 72;
+
+        // Scoot away from center in y-axis
+        if (sideTop) {
+            y += robotToCentre;
+        }
+        else{
+            y -= robotToCentre;
+        }
+
+        x += Math.cos(yaw/2) * ((robotToCentre * Math.cos(yaw)) / (Math.sin(90-(yaw/2))));
+        y += Math.cos(yaw/2) * ((robotToCentre * Math.sin(yaw)) / (Math.sin(90-(yaw/2))));
+
+        return new double[]{x, y};
     }
 }
